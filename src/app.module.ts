15410766f5ec85ddc39648c5c4255873e9f2634e
gotoAbraxas <lty7085@gmail.com>
2023-08-20 23:25:19 +0900

    토큰 받아오기성공

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,12 @@ import { DatabaseModule } from './database/database.module'
 import { SubjectsModule } from './subjects/subjects.module';
 import { JwtModule } from '@nestjs/jwt';
 
-
+/// jwt를 전역설정함으로서, 다른곳에서 서비스만 불러서 쓰게하는것.
 @Module({
   imports: [MembersModule, PostsModule, SubjectsModule, JwtModule.register({
     global: true,
-    secret: "jwtConstants.secretawefawefvawefawe",
-    signOptions: { expiresIn: '60s' },
+    secret: "jwtConstants.secretawefawefvawefawe", // 이 키는 환경변수로 등록해줘야함.
+    signOptions: { expiresIn: '2h' },
   }),
 ],
   controllers: [AppController],
--- src/members/members.controller.ts
+++ src/members/members.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, Request } from '@nestjs/common';
 import { MembersService } from './members.service';
 import { CreateMemberDto } from './dto/create-member.dto';
 import { UpdateMemberDto } from './dto/update-member.dto';
 import { LoginMemberDto } from './dto/login-member.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+
 
 @Controller('members')
 export class MembersController {
@@ -19,7 +21,20 @@ export class MembersController {
   }
 
   @Get()
-  findAll() {
+  @UseGuards(AuthGuard)
+  findAll(@Request() request) {
+
+    const test2 = request.member // 위에서 내려준거 바당온거임
+
+    console.log(test2)
+
+
+    console.log(request.header('Authorization'))
+    const test = request.headers
+
+
+    // console.log(test,'받아와지나?')
+
     return this.membersService.findAll();
   }
 
